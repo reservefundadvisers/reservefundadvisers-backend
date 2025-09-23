333980b7a6218d6dab2e7b962a6191df06b0d6c6
hitenchotaliya-ab <hc@abovestandard.dk>
2025-09-23 16:34:56 +0530

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'absdwordpress2/reservefundadvisers-backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0df25b6e9ec4688ac4efe4732da7b6db0d3c30bf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'absdwordpress2/reservefundadvisers-backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0df25b6e9ec4688ac4efe4732da7b6db0d3c30bf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'simplon/mysql' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'reference' => '3086ff142c45096f32592a26007484f7ff5da2dd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../simplon/mysql',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
