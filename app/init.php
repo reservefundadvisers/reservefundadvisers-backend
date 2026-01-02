bfc643598834517de3cd87712ff0fa586ad8ad03
hitenchotaliya-ab <hc@abovestandard.dk>
2026-01-02 12:00:43 +0530

    display error off

--- app/init.php
+++ app/init.php
@@ -24,7 +24,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 // ERROR REPORTING
 //------------------
 //error_reporting(0); 
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 //ini_set('display_startup_errors', 0);
 ini_set("log_errors", 1);
 //error_log( "Hello, errors!" );
