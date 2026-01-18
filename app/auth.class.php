2e26f3f6e7ebbf89373c40888289a606c0dfbb64
pm-abovestandard <pm@abovestandard.dk>
2026-01-18 16:49:27 +0530

    email send issue fix

--- app/auth.class.php
+++ app/auth.class.php
@@ -1125,7 +1125,7 @@ class Auth
 		}
 
 		// --- Fetch user details ---
-		$user_details = get_element('users', ['row_id' => $user_id]);
+		$user_details = get_element('users', ['id' => $user_id]);
 		// Some callers/passengers return the numeric auto-increment row id (e.g. 614)
 		// while the `users.id` column is a varchar. Try the `row` column fallback.
 		// if (empty($user_details) && is_numeric($user_id)) {
