27115484c0a4bb370f35ac93d205dc35a42a4c9f
pm-abovestandard <pm@abovestandard.dk>
2026-01-18 16:41:25 +0530

    Update database configuration in server.php

--- app/server.php
+++ app/server.php
@@ -53,9 +53,10 @@ $modules_dir = "$app_root/modules";
 // MySQL Configuration :
 // ------------------------
 
-$db_config['host'] = $_ENV['DB_HOST'] ?? "127.0.0.1";
-$db_config['user'] = $_ENV['DB_USER'] ?? "root";
-$db_config['pass'] = $_ENV['DB_PASS'] ?? "";
+
+$db_config['host'] = $_ENV['DB_HOST'] ?? "localhost";
+$db_config['user'] = $_ENV['DB_USER'] ?? "reservefundadvisers";
+$db_config['pass'] = $_ENV['DB_PASS'] ?? "Reservefundadvisers@123";
 $db_config['name'] = $_ENV['DB_NAME'] ?? "orloff_v_1.1.1";
 
 // $db_config['host'] = "127.0.0.1";
